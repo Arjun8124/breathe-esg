a72e63c388bd97c1c36a062a442944d0771fcdd3
Arjun8124 <gupta.arjun.8124@gmail.com>
2026-05-24 16:38:05 +0530

    Add FastAPI setup and define NormalizedRecord model

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+
+import models
+from database import engine
+
 app = FastAPI()
+models.Base.metadata.create_all(engine)
+
 
 @app.get("/")
 def root():
-    return {"message" : "Backend is running!"}
+    return {"message": "Backend is running!"}
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
 from database import Base
 
-class 
+
+class NormalizedRecord(Base):
+    __tablename__ = "normalized_record"
+    id = Column(Integer, primary_key=True, index=True)
+    source_type = Column(String)
+    activity_type = Column(String)
+    quantity = Column(Integer)
+    unit = Column(String)
+    status = Column(String)
+    is_suspicious = Column(Boolean)

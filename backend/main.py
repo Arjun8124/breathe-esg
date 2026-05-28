6a16496a7ddb055650998070ae0214936abdf0b4
Arjun8124 <gupta.arjun.8124@gmail.com>
2026-05-28 19:28:17 +0530

    added production base frontend link to the backend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ models.Base.metadata.create_all(engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://breathe-esg-wheat.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
